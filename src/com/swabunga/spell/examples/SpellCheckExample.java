6bb7ef93210be813859a021f2dfd7c902eeacf05
namanagrawal <namanagrawal@WRHNDEV063.arcsindia.org>
2017-06-09 14:48:48 +0530

    Tested for one hard coded URL and working fine.

--- src/com/swabunga/spell/examples/SpellCheckExample.java
+++ src/com/swabunga/spell/examples/SpellCheckExample.java
@@ -1,9 +1,12 @@
 package com.swabunga.spell.examples;
 
 import java.io.*;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 import com.swabunga.spell.event.*;
 import com.test.run.GetAllTextofWebpage;
+import com.google.common.io.Files;
 import com.swabunga.spell.engine.*;
 
 /** This class shows an example of how to use the spell checking capability.
@@ -14,7 +17,12 @@ public class SpellCheckExample implements SpellCheckListener {
 
   private static String dictFile = "dict/english.0";
   private SpellChecker spellCheck = null;
-
+  private static final String FILENAME = "D:\\filename.txt";
+  BufferedWriter bw = null;
+  FileWriter fw = null;
+  File file = new File(FILENAME);
+  ArrayList<String> al=new ArrayList<String>();
+ 
 
   public SpellCheckExample(String text) {
     try {
@@ -39,14 +47,58 @@ public class SpellCheckExample implements SpellCheckListener {
   }
 
   public void spellingError(SpellCheckEvent event) {
+	 
+	  try {
+			fw = new FileWriter(file.getAbsoluteFile(),true);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+			bw = new BufferedWriter(fw);  
     List suggestions = event.getSuggestions();
     if (suggestions.size() > 0) {
-      System.out.println("MISSPELT WORD: "+event.getInvalidWord());
+      //System.out.println("MISSPELT WORD: "+event.getInvalidWord());
+     
+      if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+      
+      //-----------------------
+      try{
+      	
+		bw.write("MISSPELT WORD: "+event.getInvalidWord());
+		bw.newLine();
+      }catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+      
+      //--------------------------
       for (Iterator suggestedWord=suggestions.iterator(); suggestedWord.hasNext();) {
-        System.out.println("Suggested Word: ="+suggestedWord.next());
+        //System.out.println("Suggested Word: ="+suggestedWord.next());
+        
       }
     }
     //Null actions
+    try {
+    if (bw != null)
+		bw.close();
+    
+
+	if (fw != null)
+		fw.close();
+	
+    }catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    
+    
   }
 
  // public static void main(String[] args) {
