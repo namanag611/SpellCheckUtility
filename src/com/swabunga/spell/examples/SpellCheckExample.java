1ddc8778e09f9f6cebfd21d23529f3b4d6f1999d
namanagrawal <namanag611@gmail.com>
2017-06-10 11:39:15 +0530

    Code is working fine.

--- src/com/swabunga/spell/examples/SpellCheckExample.java
+++ src/com/swabunga/spell/examples/SpellCheckExample.java
@@ -1,107 +1,116 @@
 package com.swabunga.spell.examples;
 
-import java.io.*;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.*;
-import com.swabunga.spell.event.*;
-import com.test.run.GetAllTextofWebpage;
-import com.google.common.io.Files;
-import com.swabunga.spell.engine.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.List;
 
-/** This class shows an example of how to use the spell checking capability.
+import com.swabunga.spell.engine.SpellDictionary;
+import com.swabunga.spell.event.SpellCheckEvent;
+import com.swabunga.spell.event.SpellCheckListener;
+import com.swabunga.spell.event.SpellChecker;
+import com.swabunga.spell.event.StringWordTokenizer;
+
+/**
+ * This class shows an example of how to use the spell checking capability.
  *
  * @author Jason Height (jheight@chariot.net.au)
  */
 public class SpellCheckExample implements SpellCheckListener {
 
-  private static String dictFile = "dict/english.0";
-  private SpellChecker spellCheck = null;
-  private static final String FILENAME = "D:\\filename.txt";
-  BufferedWriter bw = null;
-  FileWriter fw = null;
-  File file = new File(FILENAME);
-  ArrayList<String> al=new ArrayList<String>();
- 
+	String dir = System.getProperty("user.dir");
+	String file_path = dir + "\\spell\\";
 
-  public SpellCheckExample(String text) {
-    try {
-      SpellDictionary dictionary = new SpellDictionary(new File(dictFile));
+	private static String dictFile = "dict/english.0";
+	private SpellChecker spellCheck = null;
+	BufferedWriter bw = null;
+	FileWriter fw = null;
+	File file;
 
-      spellCheck = new SpellChecker(dictionary);
-      spellCheck.addSpellCheckListener(this);
-      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-      boolean run=true;
-      while (run) {
-	System.out.print("Enter text to spell check: ");
-	String line = text;
-	//String line=in.readLine();
-	if (line.length() <=0)
-	  break;
-		spellCheck.checkSpelling( new StringWordTokenizer(line) );
-		run=false;
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+	public SpellCheckExample(String text, int i) {
 
-  public void spellingError(SpellCheckEvent event) {
-	 
-	  try {
-			fw = new FileWriter(file.getAbsoluteFile(),true);
+		String FILENAME = file_path + i + ".txt";
+		file = new File(FILENAME);
+		try {
+			SpellDictionary dictionary = new SpellDictionary(new File(dictFile));
+
+			spellCheck = new SpellChecker(dictionary);
+			spellCheck.addSpellCheckListener(this);
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			boolean run = true;
+			while (run) {
+				// System.out.print("Enter text to spell check: ");
+				String line = text;
+				// String line = in.readLine();
+				if (line.length() <= 0)
+					break;
+				spellCheck.checkSpelling(new StringWordTokenizer(line));
+				run = false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void spellingError(SpellCheckEvent event) {
+
+		try {
+			fw = new FileWriter(file.getAbsoluteFile(), true);
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-			bw = new BufferedWriter(fw);  
-    List suggestions = event.getSuggestions();
-    if (suggestions.size() > 0) {
-      //System.out.println("MISSPELT WORD: "+event.getInvalidWord());
-     
-      if (!file.exists()) {
+		bw = new BufferedWriter(fw);
+		List suggestions = event.getSuggestions();
+		if (suggestions.size() > 0) {
+			// System.out.println("MISSPELT WORD: " + event.getInvalidWord());
+
+			if (!file.exists()) {
+				try {
+					file.createNewFile();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			// -----------------------
 			try {
-				file.createNewFile();
+
+				bw.write("MISSPELT WORD: " + event.getInvalidWord());
+				bw.newLine();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+
+			// --------------------------
+			for (Iterator suggestedWord = suggestions.iterator(); suggestedWord.hasNext();) {
+				// System.out.println("Suggested Word: =" +
+				// suggestedWord.next());
+				String message = "Suggested Word: =" + suggestedWord.next();
+			}
 		}
-      
-      //-----------------------
-      try{
-      	
-		bw.write("MISSPELT WORD: "+event.getInvalidWord());
-		bw.newLine();
-      }catch (IOException e) {
+		// Null actions
+		try {
+			if (bw != null)
+				bw.close();
+
+			if (fw != null)
+				fw.close();
+
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-      
-      //--------------------------
-      for (Iterator suggestedWord=suggestions.iterator(); suggestedWord.hasNext();) {
-        //System.out.println("Suggested Word: ="+suggestedWord.next());
-        
-      }
-    }
-    //Null actions
-    try {
-    if (bw != null)
-		bw.close();
-    
 
-	if (fw != null)
-		fw.close();
-	
-    }catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
 	}
-    
-    
-  }
 
- // public static void main(String[] args) {
-  // new SpellCheckExample();
-  //}
+	// public static void main(String[] args) {
+	/// new SpellCheckExample();
+	// }
 }
--- src/com/test/run/GetAllTextofWebpage.java
+++ src/com/test/run/GetAllTextofWebpage.java
@@ -1,39 +1,137 @@
 package com.test.run;
 
+import java.io.File;
+import java.util.ResourceBundle;
+
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.swabunga.spell.examples.SpellCheckExample;
 
-public class GetAllTextofWebpage {
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
 
+public class GetAllTextofWebpage {
+	boolean bSuccess = false;
 
 	WebDriver wd;
 	public String text;
+	ResourceBundle rb = ResourceBundle.getBundle("Elements");
+	Logger log = Logger.getLogger("devpinoyLogger");
+
+	String dir = System.getProperty("user.dir");
+	String file_path = dir + rb.getString("datafile");
+	String sDriverPath = dir + rb.getString("driverfolder");
+
+	@Parameters("browser")
 	@BeforeMethod
-	public void start()
-	{
-		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\AutomationTesting\\Drivers\\chromedriver_win32\\chromedriver.exe");
-		wd = new ChromeDriver();
-		wd.get("https://www.pre-employ.com/");
+	public void start(String browser) {
+		log.debug("************Spell Check Utility Started***************");
+		deleteSpellFiles();
+		if (browser.equalsIgnoreCase("chrome")) {
+			System.setProperty(rb.getString("chromeproperty"), sDriverPath + "\\chromedriver.exe");
+			wd = new ChromeDriver();
+			log.debug("Spell check started in chrome browser");
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			System.setProperty(rb.getString("firefoxproperty"), sDriverPath + "\\geckodriver.exe");
+			wd = new FirefoxDriver();
+			log.debug("Spell check started in firefox browser");
+		} else if (browser.equalsIgnoreCase("ie")) {
+			System.setProperty(rb.getString("ieproperty"), sDriverPath + "\\IEDriverServer.exe");
+			wd = new InternetExplorerDriver();
+			log.debug("Spell check started in internet explorer browser");
+		} else {
+			log.debug("Incorrect browser is specified in testng.xml file.");
+			log.debug("************Spell Check Utility Stopped***************");
+			Assert.fail();
+		}
 	}
-	
+
 	@Test
-	public void getalltext()
-	{
-		text=wd.findElement(By.tagName("body")).getText();
-		text = text.replace("\n", " ").replace("\r", "");
-		new SpellCheckExample(text);
+	public void getalltext() {
+		File f = new File(file_path);
+		try {
+			int i;
+			Workbook wb = Workbook.getWorkbook(f);
+			Sheet s1 = wb.getSheet(0);
+			int columns = s1.getColumns();
+			int rows = s1.getRows();
+
+			for (i = 1; i < rows; i++) {
+				String url = s1.getCell(1, i).getContents();
+				wd.get(url);
+				text = wd.findElement(By.tagName("body")).getText();
+				text = text.replace("\n", " ").replace("\r", "");
+				try {
+					new SpellCheckExample(text, i);
+					bSuccess = true;
+				} catch (Exception e) {
+
+					e.printStackTrace();
+					bSuccess = false;
+				}
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
-	
+
 	@AfterMethod
-	public void end()
-	{
+	public void end() {
 		wd.close();
 	}
 
+	public void GenerateResult(int i, boolean success) {
+		try {
+			Workbook workbook = Workbook.getWorkbook(new File(file_path));
+			// create a new excel and copy from existing
+			WritableWorkbook copy = Workbook.createWorkbook(new File(file_path), workbook);
+			WritableSheet sheet = copy.getSheet(0);
+			if (success == true) {
+				String FileCreated = "Yes";
+				Label label = new Label(2, i, FileCreated);
+				sheet.addCell(label);
+			}
+			if (success == false) {
+				String FileCreated = "No";
+				Label label = new Label(2, i, FileCreated);
+				sheet.addCell(label);
+			}
+			copy.write();
+			copy.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			log.debug("Exception Occured while generating the result: " + e.getMessage());
+		}
+	}
+
+	public void deleteSpellFiles() {
+		boolean bFilefound = false;
+		File folder = new File(".//spell");
+		log.debug("Deleting old txt files from spell folder.");
+		for (File file : folder.listFiles()) {
+			bFilefound = true;
+			if (file.getName().endsWith(".txt")) {
+				file.delete();
+				log.debug("File deleted: " + file.getName());
+			}
+		}
+		if (bFilefound == false) {
+			log.debug("No txt file was found in spell folder.");
+		}
+	}
 }
